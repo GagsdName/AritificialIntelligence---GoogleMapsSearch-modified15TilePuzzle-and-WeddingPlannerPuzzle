a4f7a543296359c01db6022a3647a90bd492de56
singhgag <singhgag@indiana.edu>
2016-09-16 22:09:01 -0400

    bfs unidirectional complete
    tested and complete bfs in a single direction

--- problem1/problem1_solution.py
+++ problem1/problem1_solution.py
@@ -1,5 +1,5 @@
-#library for parsing command line arguments
-import argparse
+#library for getting command line arguments
+import sys
 
 
 city_gps={}
@@ -12,6 +12,7 @@ def is_goal(s, endCityName):
 	#print   "\nis goal key", key
 	keySplitList = key.split('|')
 	if keySplitList[1] == endCityName:
+		s[key]["pathToNode"].append([endCityName])
 		return True
 	
 	return False
@@ -21,46 +22,56 @@ def successors(dict):
 	ret=[]
 	path = []
 	#print   dict
-	parent_key, parent_value = dict.popitem()
+	keys = list(dict.keys())
+	parent_key = keys[0]
+	parent_value = dict[parent_key]
+	#print "parent_key",parent_key
 	
 	parent_item = {parent_key: parent_value}
-	print "parent_item", parent_item
+	#print "parent_item", parent_item
 	parent_path = parent_item[parent_key]["pathToNode"]
-	print "parent_path", parent_path
+	#print "parent_path", parent_path
 	parentKeySplitList = parent_key.split('|')
-	print "parentKeySplt", parentKeySplitList
+	#print "parentKeySplt", parentKeySplitList
 	
 	#print   "keySplit",keySplitList[1]
 	for child_key, child_value in road_segments.iteritems():   # iter on both keys and values
 		if child_key.startswith(parentKeySplitList[1]):
 			child_item = {child_key:child_value}
 			childKeySplitList = child_key.split('|')
-			child_item[child_key]["pathToNode"].append(parent_path+childKeySplitList[0])
-			child_item[child_key]["costToNode"] = dict[parent_key]["costToNode"] +  child_item[child_key]["distance"]		
+			child_item[child_key]["pathToNode"].append(parent_path+[childKeySplitList[0]])
+			#print "haha:",parent_key, "child_key", child_key
+			child_item[child_key]["costToNode"] = float(dict[parent_key]["costToNode"]) +  float(child_item[child_key][routeOptionString])	
+			
 			#print   "\nkey:value", key,value
-			ret.append({key:value})
+			ret.append({child_key:child_value})
 		
-	print  "\nret", ret
+	#print  "\nret", ret
 	return ret
 
 
 
 
-def bfs(startCity,endCity,routeOption):
+def bfs():
 	
 	fringe = []
+	global goal_cost
+	global goal_path
+	goal_path=""
+	goal_cost=9999999999
 	
-	startCityName = str(startCity).strip('[]')
-	endCityName = str(endCity).strip('[]')
 	#print  startCityName
 	
 	for key, value in road_segments.iteritems():   # iter on both keys and values
+		#print key
 		if key.startswith(startCityName):
 			item  = {key:value}
+			#print item
 			keySplitList = key.split('|')
 			item[key]["pathToNode"].append(startCityName)
-			item[key]["pathToNode"].append(keySplitList[1])
-			item[key]["costToNode"] = item[key]["distance"]			
+			item[key]["costToNode"] = item[key][routeOptionString]	
+			#print "String",routeOptionString, "key",key
+			#print "distance of bappa ",item[key]["costToNode"]
 			fringe.append(item)
 			
 	
@@ -74,23 +85,35 @@ def bfs(startCity,endCity,routeOption):
 			#print  "\n\n\nfringe after pop", fringe
 			#print  "state", s
 			if is_goal(s,endCityName):
-				print  "goal", s
-				return
+				keys = list(s.keys())
+				s_key = keys[0]
+				float_val = float(s[s_key]["costToNode"])
+				#print goal_cost
+				if float_val < goal_cost:
+					goal_cost = float(s[s_key]["costToNode"])
+					goal_path = s[s_key]["pathToNode"]
+					continue
+			
+			
 			if s not in fringe:
 				#print  "nahi"
 				fringe.append(s)
 				
+	
 	return False
 
 	
 #function to suggest good driving directions 
 def get_driving_directions(startCity,endCity,routingOption,routingAlgorithm):
 	
-	
+	global startCityName, endCityName, routeOptionString
+	startCityName = str(startCity).strip('[]')
+	endCityName = str(endCity).strip('[]')
+	routeOptionString = str(routingOption).strip('[]')
+	#print "intiial routing option",routingOption, "string", routeOptionString
 	#If routing algorithm is astar
 	if "bfs" in routingAlgorithm:
-		if "distance" in routingOption:
-			bfs(startCity, endCity, routingOption)
+			bfs()
 			
 	
 	
@@ -121,21 +144,28 @@ def readFiles():
 			if len(lineSplit) == 5 and int(lineSplit[2]) and int(lineSplit[3]):
 				#print   "distance",lineSplit[2],"speed",lineSplit[3]
 				road_segments.update({lineSplit[0]+"|"+lineSplit[1]+"|"+lineSplit[4]:{"distance": lineSplit[2], "speed": lineSplit[3],\
-					"isHighway":1 if lineSplit[3] >=55 else 0, "travelTime": float(lineSplit[2])/float(lineSplit[3]), "pathToNode":[], "costToNode":-1}}); 
+					"isHighway":1 if lineSplit[3] >=55 else 0, "travelTime": float(lineSplit[2])/float(lineSplit[3]), "pathToNode":[], "costToNode":0}}); 
 			''' TO-DO: handle else case '''
 			
 	#print   road_segments
 	
 readFiles()
 #parsing command line arguments
-cl_parser = argparse.ArgumentParser()
-cl_parser.add_argument("startCity", help="Enter the Starting Point of your journey")
-cl_parser.add_argument("endCity", help="Enter the Ending Point of your journey")
-cl_parser.add_argument("routingOption", help="Enter the Routing Option - One of Segments, Distance, Time or Scenic ")
-cl_parser.add_argument("routingAlgorithm", help="Enter the Routing Algorithm - One of bfs, dfs, iterative deepening or astar")
-cl_args = cl_parser.parse_args() 
+# cl_parser = argparse.ArgumentParser()
+# cl_parser.add_argument("startCity", help="Enter the Starting Point of your journey")
+# cl_parser.add_argument("endCity", help="Enter the Ending Point of your journey")
+# cl_parser.add_argument("routingOption", help="Enter the Routing Option - One of Segments, Distance, Time or Scenic ")
+# cl_parser.add_argument("routingAlgorithm", help="Enter the Routing Algorithm - One of bfs, dfs, iterative deepening or astar")
+# cl_args = cl_parser.parse_args() 
 #print  cl_args.startCity
-get_driving_directions(cl_args.startCity, cl_args.endCity, cl_args.routingOption, cl_args.routingAlgorithm)
+startCity=sys.argv[1]
+endCity=sys.argv[2]
+routingOption=sys.argv[3]
+routingAlgorithm=sys.argv[4]
+
+get_driving_directions(startCity,endCity,routingOption,routingAlgorithm)
+print "\npath:",goal_path," \ncost:",goal_cost
+
 
 
 	
